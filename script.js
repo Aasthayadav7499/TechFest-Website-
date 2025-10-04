0ea1d54f9df1dc5c3e317dc989f2e20af773dbe4
Aastha Yadav <aasthayadav001delhi@gmail.com>
2025-10-04 18:37:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
+document.getElementById("registrationForm").addEventListener("submit", function(event){
   event.preventDefault();
 
   let name = document.getElementById("name").value.trim();
   let email = document.getElementById("email").value.trim();
   let college = document.getElementById("college").value.trim();
-  let eventSelected = document.getElementById("event").value;
+  let eventSelected = document.getElementById("eventSelected").value.trim();
 
   let message = "";
-
   // Simple Validation
-  if (name.length < 3) {
+  if(name.length<3){
     message = "Name must be at least 3 characters.";
-  } else if (!email.includes("@")) {
+  } else if (!email.includes("@")){
     message = "Enter a valid email.";
-  } else if (college.length < 2) {
+  } else if (college.length<2){
     message = "College name must be valid.";
-  } else if (eventSelected === "") {
+  } else if (eventSelected === ""){
     message = "Please select an event.";
   } else {
     message = "✅ Registration successful! Thank you for registering.";
     document.getElementById("registrationForm").reset();
   }
 
-  document.getElementById("formMessage").innerText = message;
+  document.getElementById("FormMessage").reset();
 });
